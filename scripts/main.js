07ba3f5a66540446fa353a4af10e8caf9bcdfa6d
Nikolay Sitnikov <nicholasitnikov@gmail.com>
2021-08-05 12:25:09 +0500

    Перемещение массива карточек по-умолчанию

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,4 @@
 
-// Список карточек, добавляемых по-умолчанию
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]; 
-
-
 // Создание переменных для блоков и элементов
 
 const buttonEdit = document.querySelector('.profile__edit');
